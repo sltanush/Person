62946a2a0cc4ce329fdbd7ae101dbfb8209746cf
sltanush <sltanush77@gmail.com>
2023-06-03 12:00:15 +0530

    first commit

---
+++ src/main/java/com/ty/person/Person.java
@@ -0,0 +1,43 @@
+package com.ty.person;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+	@Id
+	private int id;
+	private String name;
+	private String address;
+	private long phno;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public long getPhno() {
+		return phno;
+	}
+	public void setPhno(long phno) {
+		this.phno = phno;
+	}
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", address=" + address + ", phno=" + phno + "]";
+	}
+	
+	
+}
